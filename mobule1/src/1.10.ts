25c53a8558f845b9240140195bedde9aed0e0df5
Md Sujon Mia <sujon258549@gmail.com>
2024-10-31 23:09:52 +0600

    modified 1.10.ts in src

--- mobule1/src/1.10.ts
+++ mobule1/src/1.10.ts
@@ -16,10 +16,32 @@
         bladgroup: 'A+' | 'B+' | 'C+'
     }
 
-        const useInfo:UserInfo ={
-            name:'sujon',
-            fatherName:'Nazrul Islam',
-            mobile:1790876529,
-            
-        }
+    const useInfo: UserInfo = {
+        name: 'sujon',
+        fatherName: 'Nazrul Islam',
+        mobile: 1790876529,
+        bladgroup: 'A+'
+
+    }
+    // inter septor using for typescript
+    type Userrole = {
+        userSkills: string[];
+        normalUseer: 'isUser';
+        userName: string
+    }
+    type Admin ={
+        userSkills:string[];
+        adminUser:'isAdmin';
+        userName:string
+    }
+
+    type Mainadmin = Userrole & Admin
+
+    const mainadmin:Mainadmin ={      // mast be all Main admin user role example userole and admin  all data writin no data writing error this time typescritr !!
+        userSkills:['goodCommunicaitn', 'timework'],
+        normalUseer:'isUser',
+        userName:'Md Sujon Mia',
+        adminUser:'isAdmin',
+
+    }
 }
