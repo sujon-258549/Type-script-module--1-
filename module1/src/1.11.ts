ee8d1fda8a0610660e73d8ffb7b4ca186d8dcf00
Md Sujon Mia <sujon258549@gmail.com>
2024-10-31 23:32:33 +0600

    modified 1.11.ts in src

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -8,4 +8,11 @@
     else{
         console.log("it is no adalt")
     }
-}
+
+    const isAdalt = old>= 18 ? console.log('it is adalt') : console.log(' it is no adalt')
+
+    // nalis qualifing operator
+    // How to use nalis qualification operrator use nul and undefine 
+    const data:null = null;
+    const gest = data?? 'gest'
+} 
