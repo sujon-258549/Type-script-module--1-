adc43b7305abe569cc43f25cf7cd8d3f957d5883
Md Sujon Mia <sujon258549@gmail.com>
2024-10-31 23:22:04 +0600

    changed 37 files

---
+++ module1/src/1.11.ts
@@ -0,0 +1,11 @@
+{
+    // tarnary operator || optonal caining || and 
+    // 
+    let old:number = 22;
+    if(old >= 18){
+        console.log('It is adalt')
+    }
+    else{
+        console.log("it is no adalt")
+    }
+}
