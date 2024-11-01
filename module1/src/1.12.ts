cd39b32ad33e0bdb4c00936c9c0cc06f36bd7ef2
Md Sujon Mia <sujon258549@gmail.com>
2024-11-01 11:53:02 +0600

    changed 3 files

---
+++ module1/src/1.12.ts
@@ -0,0 +1,10 @@
+{
+    const search = (value: string | null) => {
+        if (value) {
+            console.log('serching')
+        } else {
+            console.log('pleace inter your value')
+        }
+    }
+    search(null)
+}
