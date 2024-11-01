b22898bdf4e27ec2af1852e512883788e330c233
Md Sujon Mia <sujon258549@gmail.com>
2024-11-01 12:54:27 +0600

    modified 1.12.ts in src

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,10 +1,35 @@
 {
     const search = (value: string | null) => {
         if (value) {
-            console.log('serching')
+            // console.log('serching')
         } else {
-            console.log('pleace inter your value')
+            // console.log('pleace inter your value')
         }
     }
     search(null)
+
+    const data = (scoremetar: unknown) => {
+
+        if (typeof scoremetar === 'number') {
+            const mainData = scoremetar * scoremetar
+            console.log(`${mainData} Scoremetor`)
+        }
+        if (typeof scoremetar === 'string') {
+            const [newData, data] = scoremetar.split(' ')
+            const finaldata = (parseFloat(newData) * parseFloat(newData))
+            console.log(finaldata)
+        }
+        else {
+            console.log('Pleace Inter Your valid Data')
+        }
+    }
+    data("10 mitor")
+
+    const errorMessage = (msg: string) => {
+
+        throw new Error(msg);
+        
+
+    }
+    errorMessage('thisis error')
 }
