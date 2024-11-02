ffe99f033e7b5de58af6b2def18a8529fa312ca8
Md Sujon Mia <sujon258549@gmail.com>
2024-11-02 22:48:26 +0600

    modified 2.7.ts in src

--- module2/src/2.7.ts
+++ module2/src/2.7.ts
@@ -24,24 +24,39 @@
     const parson1: onner2 = 'car'
 
 
-    const user = {
-        name: "sujon",
-        age: 23,
-        education: 'diploma',
-        phone: 1790876529
+    // const user = {
+    //     name: "sujon",
+    //     age: 23,
+    //     education: 'diploma',
+    //     phone: 1790876529
+    // }
+
+    // user['name']
+    // console.log(user["age"])   
+
+
+    // const objectData = <X, Y extends keyof X>(obj: X, kye: Y) => {
+    //     console.log(obj[kye])
+    //     return obj[kye]
+    // }
+
+    // const postobjectData = objectData(user, "age")
+
+
+    const user2 = {
+        name: 'ami',
+        home: 'ran',
+        phone: 115,
     }
 
-    user['name']
-    console.log(user["age"])
-
-
-    const objectData = <X, Y extends keyof X>(obj: X, kye: Y) => {
-        console.log(obj[kye])
-        return obj[kye]
+    const userGetData = <A, B extends keyof A>(objs: A, keys: B) => {
+        return objs[keys]
     }
 
-    const postobjectData = objectData(user, "age")
+    const result = userGetData(user2,'phone')
 
+console.log(result)
+    
     // 
     // 
 }
