43da7286fd09dc25da7329c7e62ffa927a8e8812
Md Sujon Mia <sujon258549@gmail.com>
2024-11-02 22:35:04 +0600

    modified 2.7.ts in src

--- module2/src/2.7.ts
+++ module2/src/2.7.ts
@@ -1,6 +1,5 @@
 {
     //////////////////
-    
 
 
 
@@ -8,13 +7,40 @@
 
 
 
-// kyeof operator
+
+    // kyeof operator
+
+    type Vehical = {
+        bike: string;
+        car: string;
+        sipe: string
+    }
+
+    //  kye of use no manualy write
+    type onner1 = "biky" | "car" | "sipe";
+    type onner2 = keyof Vehical;
 
 
+    const parson1: onner2 = 'car'
 
 
+    const user = {
+        name: "sujon",
+        age: 23,
+        education: 'diploma',
+        phone: 1790876529
+    }
+
+    user['name']
+    console.log(user["age"])
 
 
+    const objectData = <X, Y extends keyof X>(obj: X, kye: Y) => {
+        console.log(obj[kye])
+        return obj[kye]
+    }
+
+    const postobjectData = objectData(user, "age")
 
     // 
     // 
