0527fbc333eeccefb1c9e5bc237d4f6806c2d7ca
Md Sujon Mia <sujon258549@gmail.com>
2024-11-02 23:35:03 +0600

    modified 2.8.ts in src

--- module2/src/2.8.ts
+++ module2/src/2.8.ts
@@ -1,10 +1,10 @@
 {
     //promice
-     type Type = string
+    type Type = string
 
-    const createPromice = ():Promise<Type> => {
+    const createPromice = (): Promise<Type> => {
         return new Promise(((resolve, reject) => {
-            const data:Type = 'hellow I,m Data';
+            const data: Type = 'hellow I,m Data';
             if (data) {
                 resolve(data)
             }
@@ -14,13 +14,52 @@
         }))
     }
 
-const  collCreatePromice = async():Promise<Type>=>{
-    const result:Type = await createPromice()
-    console.log(result)
-    return result
-}
+    const collCreatePromice = async (): Promise<Type> => {
+        const result: Type = await createPromice()
+        // console.log(result)
+        return result
+    }
 
-collCreatePromice()
+    collCreatePromice()
+
+
+    //  use json pleaceholder
+
+    interface Usertype {
+        userId: number;
+        id: number;
+        title: string;
+        completed: boolean
+    }
+    // prumise use must be use type 
+    const useJsonPlaceholder = async (): Promise<Usertype> => {
+        const resoponse = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const result2:Usertype = await resoponse.json();
+        return result2
+    }
+    const logPostUser = async () => {
+        const postUser = await useJsonPlaceholder();
+        console.log(postUser);
+    };
+
+    logPostUser()
+
+
+
+    // const useJsonPlaceholder = async (): Promise<Usertype> => {
+    //     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    //     const result: Usertype = await response.json();
+    //     return result;
+    // };
+    
+    // // Use async function to log the resolved data
+    // const logPostUser = async () => {
+    //     const postUser = await useJsonPlaceholder();
+    //     console.log(postUser);
+    // };
+    
+    // logPostUser();
+    
 
     // 
 }
