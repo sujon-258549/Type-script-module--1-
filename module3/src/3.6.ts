07ce9ea3359eca2e49a83f2ce8f0191bd4f5f828
Md Sujon Mia <sujon258549@gmail.com>
2024-11-05 13:12:07 +0600

    modified 3.6.ts in src

--- module3/src/3.6.ts
+++ module3/src/3.6.ts
@@ -4,6 +4,26 @@
 
 
 
+    // access modifire
+
+    class Bank {
+        constructor(readonly id: number, public name: string, private _balance: number) {
+
+        }
+
+        get Balanse() {
+            return console.log(this._balance)
+        }
+        set Balanses(input: number) {
+            console.log(this._balance += input)
+        }
+
+    }
+
+    const poorAccount = new Bank(1, 'Sujon', 50);
+    const currentBalanse = poorAccount.Balanses = 150;
+    // console.log(currentBalanse)
+
 
 
 
